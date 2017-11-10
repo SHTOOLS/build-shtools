445810e3e9184c544a35aa511bcd72cc6818f7b7
xoviat <xoviat@users.noreply.github.com>
2017-11-09 23:25:47 -0600

    [alias] add functions

--- alias.c
+++ alias.c
@@ -9,3 +9,18 @@ void dfftw_destroy_plan_(fftw_plan* plan) {
     fftw_destroy_plan(*plan);
     return;
 }
+
+void dfftw_plan_dft_r2c_1d_(fftw_plan* plan, int* n, double *in, fftw_complex *out, unsigned* flags) {
+
+    *plan = fftw_plan_dft_r2c_1d(*n, in, out, *flags);
+    return;
+}
+
+
+void dfftw_plan_dft_1d_(fftw_plan* plan, int* n, fftw_complex *in, fftw_complex *out, int* sign, unsigned* flags) {
+
+    *plan = fftw_plan_dft_1d(*n, in, out, *sign, *flags);
+
+    return;
+}
+
